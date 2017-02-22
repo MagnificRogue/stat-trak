8b50fc059bd611b6c174ee47447a4816fec47abe
Brian Ludwig <hcbl1212@batman-tool.local>
2017-02-22 04:59:40 -0800

    finished crud stuff for user

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use App\User;
+
+class UserController extends Controller{
+
+
+  protected $request;
+
+  public function __construct(Request $request) {
+    $this->request = $request;
+  }
+
+  public function index(){
+    return $data["data"]["users"] = User::all();; 
+  }
+
+  public function show(User $user){
+    return $data["data"]["user"] = $user;
+  }
+
+  public function create(){
+    if($user = User::create($this->request->only('name','email', 'password','company_id'))){
+      return $data["data"]["user"] = $user;
+    }else{
+      return response()->json(['message' => 'Record could not be created'], 400);
+    } 
+  }
+
+  public function destroy($id){
+    if(!User::destroy($id)){
+      return response()->json(['message' => 'Record not found'], 404);
+    } 
+    return $data["data"]["user"] = null;
+  }
+
+  public function update(User $user){
+    if($user->update($this->request->all())){
+     return  $data["data"]["user"] = $user;
+    }else{
+      return response()->json(['message' => 'Record could not be created'], 400);
+    } 
+  }
+
+}
--- routes/web.php
+++ routes/web.php
@@ -10,98 +10,22 @@
 | and give it the controller to call when that URI is requested.
 |
 */
-Route::get('/', function () {
-    \Session::put('lang', 'en');
-    return view('login');
+//authentication route
+Route::post("/users/sign_in",'AuthenticateController@authenticate');
+Route::group(["middleware" =>["jwt.auth"]],function(){
+  
+  //authentication route
+  Route::post('/users/sign_out','AuthenticateController@invalidate');
+  
+  //user routes
+  Route::get("/users/create/","UserController@create");
+  Route::get("/users",'UserController@index');
+  Route::get("/users/{user}",'UserController@show');
+  Route::delete("/users/{user}",'UserController@destroy');
+  Route::put("/users/{user}",'UserController@update');
 });
 
-Route::get('/login', function () {
-    return view('login');
-});
-Route::get('/signup', function () {
-    return view('signup');
-});
-Route::get('/404-page', function () {
-    return view('404-page');
-});
-Route::get('/home', function () {
-    return view('pages/home');
-});
-Route::get('/profile', function () {
-    return view('pages/profile');
-});
-Route::get('/typography', function () {
-    return view('pages/typography');
-});
-Route::get('/grid', function () {
-    return view('pages/grid');
-});
-Route::get('/table', function () {
-    return view('pages/table');
-});
-Route::get('/form-elements', function () {
-    return view('pages/forms/form-elements');
-});
-Route::get('/form-components', function () {
-    return view('pages/forms/form-components');
-});
 
-Route::get('/button', function () {
-    return view('pages/ui-element/button');
-});
-Route::get('/dropdown', function () {
-    return view('pages/ui-element/dropdown');
-});
-Route::get('/icons', function () {
-    return view('pages/ui-element/icon');
-});
-Route::get('/panels', function () {
-    return view('pages/ui-element/panel');
-});
-Route::get('/alerts', function () {
-    return view('pages/ui-element/alert');
-});
-Route::get('/progressbars', function () {
-    return view('pages/ui-element/progressbar');
-});
-Route::get('/pagination', function () {
-    return view('pages/ui-element/pagination');
-});
-Route::get('/other-elements', function () {
-    return view('pages/ui-element/other');
-});
 
-Route::get('/chartjs', function () {
-    return view('pages/charts/chartjs');
-});
-Route::get('/c3chart', function () {
-    return view('pages/charts/c3chart');
-});
-Route::get('/calendar', function () {
-    return view('pages/calendar');
-});
-Route::get('/inbox', function () {
-    return view('pages/mail/inbox');
-});
-Route::get('/compose', function () {
-    return view('pages/mail/compose');
-});
-Route::get('/invoice', function () {
-    return view('pages/invoice');
-});
-Route::get('/docs', function () {
-    return view('pages/docs');
-});
-Route::get('/blank', function () {
-    return view('pages/blank');
-});
 
-Route::get('api/change-theme', function() {
-    Session::set('theme', Request::get('theme'));
-});
-Route::get('api/lang', function() {
-    Session::set('lang', Request::get('lang'));
-});
-Route::get('api/set-rtl', function() {
-    Session::set('rtl', Request::get('rtl'));
-});
+
