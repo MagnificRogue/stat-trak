8a8c3e4f9dee0f795ac6fb0e94eec579be14d00b
Brian Ludwig <hcbl1212@batman-tool.local>
2017-02-25 16:56:17 -0800

    finished unit tests for user crud operations

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -39,4 +39,52 @@ class ExampleTest extends TestCase{
     $this->assertEquals($user["email"],$this->user["email"]);
   }
 
+
+  public function testCreateUser(){
+    //get all the users
+    $response = $this->callAuthenticated('GET', '/users');
+    $this->assertTrue($response->isOk());
+    $users = json_decode($response->content());
+
+    //create a new user 
+    $response = $this->callAuthenticated('GET', '/users/create',["name"=>"Brian Ludwig, JR","password"=>"Harper86","email"=>"faker@poop.com"]);
+    $this->assertTrue($response->isOk());
+    $user =  json_decode($response->content(),true);
+    
+    //make sure the user is new user 
+    $this->assertEquals($user["email"],"faker@poop.com");
+    $this->assertCount(sizeOf($users)+1,\App\User::all());
+  }
+
+  public function testUpdateUser(){
+    
+    //get the last user (should be the user created in the test above
+    $user_to_update = \App\User::all()->last();     
+    $this->assertEquals($user_to_update->email,"faker@poop.com");
+    
+    //create a new user 
+    $response = $this->callAuthenticated('PUT', "/users/$user_to_update->id",["email"=>"real@poop.com"]);
+    $this->assertTrue($response->isOk());
+    $user =  json_decode($response->content(),true);
+    $this->assertNotEquals($user["email"],"faker@poop.com");
+    $this->assertEquals($user["email"],"real@poop.com");
+  }
+
+  public function testDeleteUser(){
+    //get all the users
+    $response = $this->callAuthenticated('GET', '/users');
+    $this->assertTrue($response->isOk());
+    $users = json_decode($response->content());
+    
+    //get the last user (should be the user created in the test above
+    $user_to_delete = \App\User::all()->last();     
+    $this->assertEquals($user_to_delete->email,"real@poop.com");
+    
+    //delete the user created last user 
+    $response = $this->callAuthenticated('DELETE', "/users/$user_to_delete->id");
+    $this->assertTrue($response->isOk());
+    
+    $this->assertCount(sizeOf($users)-1,\App\User::all());
+  }
+
 }
