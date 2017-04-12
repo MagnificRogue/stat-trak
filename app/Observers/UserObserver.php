762d7763d41b4abccaed25986e4d2a5074e67f01
Brian Ludwig <hcbl1212@batman-tool.local>
2017-04-12 07:00:16 -0700

    changed to saving event instead of two events

--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -5,17 +5,10 @@ use App\User;
 use Illuminate\Support\Facades\Hash;
 
 class UserObserver {
-
-  public function created(User $user) {
+  public function saving(User $user) {
     if($user->isDirty('password')) {
       $user->password = Hash::make($user->password);
     }  
-  }
-
-  public function updating(User $user) {
-    if($user->isDirty('password')) {
-      $user->password = Hash::make($user->password);
-    } 
   }
 }
 
